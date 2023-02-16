f054df5e885eced7ec08dbc4bb8e9d2e202af793
Serg <serg212007@gmail.com>
2023-02-16 17:10:13 +0200

    12

--- rnNewHomework/App.js
+++ rnNewHomework/App.js
@@ -1,20 +1,11 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import LoginScreen from "./Screens/components/LoginScreen/LoginScreen";
+import RegistrationScreen from "./Screens/components/RegistrationScreen/RegistrationScreen";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Hello world!!! New product!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+      <RegistrationScreen />
+      {/* <LoginScreen /> */}
+    </>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
